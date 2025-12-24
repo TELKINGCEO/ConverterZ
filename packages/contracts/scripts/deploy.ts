733421528a623f93017bf4954f19745206b1a3d7
TELKINGCEO <159771854+TELKINGCEO@users.noreply.github.com>
2025-12-24 20:59:44 +0530

    Update deploy.ts

--- packages/contracts/scripts/deploy.ts
+++ packages/contracts/scripts/deploy.ts
@@ -1,1 +1,12 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Factory = await ethers.getContractFactory("ImageConverterFHE");
+  const contract = await Factory.deploy();
+  await contract.deployed();
+
+  console.log("ImageConverter deployed to:", contract.address);
+}
+
+main();
 
